5623cc7b20991ffaca50218f83cdeeecf878f870
Romeo Aquino <39586287+Keeeszo@users.noreply.github.com>
2025-05-14 19:05:57 -0600

    Agregando flask

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from telegram.ext import Application
 from bot.handlers import register_handlers
 from bot.jobs import check_builders_notifications
 from config import TELEGRAM_TOKEN
+from flask import Flask
+import threading
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -10,12 +12,25 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+# Crea un servidor HTTP mínimo en un puerto secundario
+app = Flask(__name__)
+
+
+@app.route('/')
+def health_check():
+    return "Bot activo", 200
+
+
+def run_flask():
+    app.run(host='0.0.0.0', port=8000)
+
 
 def main():
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.start()
+
     application = Application.builder().token(TELEGRAM_TOKEN).build()
-
     register_handlers(application)
-
     if hasattr(application, "job_queue"):
         application.job_queue.run_repeating(
             check_builders_notifications,
